875ff24444cf9be8eee42df971e83c5dcbf75016
Barbara Morales <bamo6610@colorado.edu>
2026-08-24 19:33:51 -0600

    corrected simulation setup details

--- run_nacl_FBP_membrane.sh
+++ run_nacl_FBP_membrane.sh
@@ -2,17 +2,16 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=160:00:00
+#SBATCH --time=100:00:00
 #SBATCH --partition=blanca-shirts
 #SBATCH --qos=blanca-shirts
 #SBATCH --account=blanca-shirts
 #SBATCH --gres=gpu
-#SBATCH --mem=10000m
-#SBATCH --job-name=FBP_nacl_4184
-#SBATCH --output=slurm_codes/FBP_nacl_4184.%j.log
+#SBATCH --mem=8000m
+#SBATCH --job-name=FBP_nacl_35m
+#SBATCH --output=slurm_codes/FBP_nacl_35m.%j.log
 
 module purge
-module avail
 ml anaconda
 conda activate polymerist-env
 
@@ -21,25 +20,20 @@ export RESTRAINT=FBP
 export SALT="Na Cl"
 export CENTERIONS="Na Cl"
 export WATER="TIP3P"
-export CONCENTRATIONS="0.5 1.0 1.4 2.0 2.5 3.0 3.5 4.0"
+export CONCENTRATIONS="3.5"
 export REPNUM=20
-
-# k (single); if you want multiple ks, replace with --k_values in the call below
-export FORCEK=4184
-
-# New: box geometry
-export LZ_NM=14.4     # total Z box length (nm)
-export ZCENTER_NM=7.2  # restraint center (nm); default is LZ_NM/2 if omitted
+export SALT_DATA=structures/salt_data_48x48x144.py
+export PDB_SUFFIX=""
 
 # Force fields
 export FF_WATER='tip3p.offxml'
 export FF_IONS='openff-2.3.0.offxml'
 
 # shouldn't need to change
-export CASE=${RESTRAINT}_$(echo ${SALT} | tr -d ' ')_${WATER}_4184
+export CASE=${RESTRAINT}_$(echo ${SALT} | tr -d ' ')_${WATER}
 export FF_FILES="$FF_WATER $FF_IONS"
 
-python osmotic_sim_dispatch_membar_cont.py \
+python -u osmotic_sim_dispatch_membar_cont.py \
   -n "${CASE}" \
   -s ${SALT} \
   -ci ${CENTERIONS} \
@@ -48,12 +42,17 @@ python osmotic_sim_dispatch_membar_cont.py \
   -m ${CONCENTRATIONS} \
   -ff ${FF_FILES} \
   -r ${RESTRAINT} \
-  -k ${FORCEK} \
   -du nanometer \
-  --lz_nm "${LZ_NM}" \
-  --z_center_nm "${ZCENTER_NM}" \
-  --delta_z 2.4
+  --salt_data "${SALT_DATA}" \
+  --pdb_suffix "${PDB_SUFFIX}" \
+  --temperature_K 298.15 \
+  --equil_nvt_ps 50 \
+  --equil_npt_ns 5 \
+  --prod_ns 20 \
+  --prod_samples 1000 \
+  --pressure_bar 1.01325 \
+  --surface_tension_bar_nm 0.0
 
-echo "CASE=${CASE}  k=${FORCEK}  Lz=${LZ_NM} nm  z_center=${ZCENTER_NM} nm  DATE=$(date -Iseconds)"
+echo "CASE=${CASE} Lz=${LZ_NM} nm  z_center=${ZCENTER_NM} nm  DATE=$(date -Iseconds)"
 
 sacct --format=jobid,jobname,cputime,elapsed
--- run_nacl_HP_doublebox_smoke.sh
+++ run_nacl_HP_doublebox_smoke.sh
@@ -7,12 +7,11 @@
 #SBATCH --qos=blanca-shirts
 #SBATCH --account=blanca-shirts
 #SBATCH --gres=gpu
-#SBATCH --mem=12000m
-#SBATCH --job-name=HP_nacl_db35m050smoke
-#SBATCH --output=slurm_codes/HP_nacl_db35m050smoke.%j.log
+#SBATCH --mem=6000m
+#SBATCH --job-name=HP_nacl_db35msmoke
+#SBATCH --output=slurm_codes/HP_nacl_db35msmoke.%j.log
 
 module purge
-module avail
 ml anaconda
 conda activate polymerist-env
 
@@ -23,24 +22,19 @@ export CENTERIONS="Na Cl"
 export WATER="TIP3P"
 export CONCENTRATIONS="3.5"
 export REPNUM=1
+export SALT_DATA=structures/salt_data_48x48x288_clone.py
 export PDB_SUFFIX="d"
 
-# k (single); if you want multiple ks, replace with --k_values in the call below
-export FORCEK=0.50
-
-# New: box geometry
-export LZ_NM=28.8     # total Z box length (nm)
-export ZCENTER_NM=14.4  # restraint center (nm); default is LZ_NM/2 if omitted
 
 # Force fields
 export FF_WATER='tip3p.offxml'
 export FF_IONS='openff-2.3.0.offxml'
 
 # shouldn't need to change
-export CASE=${RESTRAINT}_$(echo ${SALT} | tr -d ' ')_${WATER}_050_${PDB_SUFFIX}box_smoke
+export CASE=${RESTRAINT}_$(echo ${SALT} | tr -d ' ')_${WATER}_${PDB_SUFFIX}box_smoke
 export FF_FILES="$FF_WATER $FF_IONS"
 
-python osmotic_sim_dispatch_membar_cont.py \
+python -u osmotic_sim_dispatch_membar_cont.py \
   -n "${CASE}" \
   -s ${SALT} \
   -ci ${CENTERIONS} \
@@ -49,10 +43,8 @@ python osmotic_sim_dispatch_membar_cont.py \
   -m ${CONCENTRATIONS} \
   -ff ${FF_FILES} \
   -r ${RESTRAINT} \
-  -k ${FORCEK} \
   -du nanometer \
-  --lz_nm "${LZ_NM}" \
-  --z_center_nm "${ZCENTER_NM}" \
+  --salt_data "${SALT_DATA}" \
   --pdb_suffix "${PDB_SUFFIX}" \
   --temperature_K 298.15 \
   --equil_npt_ns 0.5 \
@@ -61,5 +53,5 @@ python osmotic_sim_dispatch_membar_cont.py \
   --pressure_bar 1.01325 \
   --surface_tension_bar_nm 0.0
 
-echo "CASE=${CASE}  k=${FORCEK}  Lz=${LZ_NM} nm  z_center=${ZCENTER_NM} nm  DATE=$(date -Iseconds)"
+echo "CASE=${CASE} Lz=${LZ_NM} nm  z_center=${ZCENTER_NM} nm  DATE=$(date -Iseconds)"
 sacct --format=jobid,jobname,cputime,elapsed
